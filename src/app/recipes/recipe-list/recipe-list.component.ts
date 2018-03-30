8bd012a87dc3c8fa668fed3b534fb353468e4376
JasonFinestone <jasonfinestone@gmail.com>
2018-03-30 18:29:35 +0100

    Progress witht he course

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {RecipeModel} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: RecipeModel[] = [
+
+    new RecipeModel('A Test Recipe', 'This is a test descrition', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--488691_11.jpg?itok=ExaTspz1'),
+    new RecipeModel('A Test Recipe', 'This is a test descrition', 'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--488691_11.jpg?itok=ExaTspz1')
+
+
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
